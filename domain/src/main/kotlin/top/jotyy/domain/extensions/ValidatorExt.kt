b3aec2ff7e767c4fa9453508c0b1cc8aa75e65ad
Jotyy <838373812@qq.com>
2020-12-08 22:14:04 +0800

    feat: make failure message better

--- domain/src/main/kotlin/top/jotyy/domain/extensions/ValidatorExt.kt
+++ domain/src/main/kotlin/top/jotyy/domain/extensions/ValidatorExt.kt
@@ -1,13 +1,15 @@
 package top.jotyy.domain.extensions
 
 import org.valiktor.ConstraintViolationException
+import org.valiktor.i18n.mapToMessage
 import top.jotyy.core.exception.Failure
 import top.jotyy.core.exception.ValidationFailure
 
 fun ConstraintViolationException.toFailure(): Failure = ValidationFailure(
     this.constraintViolations
+        .mapToMessage()
         .map {
-            "Parameter ${it.property} requires ${it.constraint.name}"
+            "Parameter ${it.property}: ${it.message}"
         }
         .first()
 )
