0561fef3662863ebdec04ffbc15277b5b54496fa
Jotyy <838373812@qq.com>
2020-12-14 15:14:55 +0800

    feat: create Relations table

--- data/src/main/kotlin/top/jotyy/data/database/table/Relations.kt
+++ data/src/main/kotlin/top/jotyy/data/database/table/Relations.kt
@@ -1,16 +1,14 @@
 package top.jotyy.data.database.table
 
-import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.jodatime.datetime
+import org.joda.time.DateTime
 
 /**
  * Relation Table
- *
- * @author Jotyy
  */
-object Relations : Table("tb_blog_tag_relation") {
-    val relationId = integer("relation_id")
-    val blogId = integer("blog_id")
-    val tagId = integer("tag_id")
-    val createdAt = datetime("created_at")
+object Relations : IntIdTable(name = "tb_blog_tag_relation", columnName = "relation_id") {
+    val blogId = reference("blog_id", Blogs.id)
+    val tagId = reference("tag_id", Tags.id)
+    val createdAt = datetime("created_at").default(DateTime())
 }
